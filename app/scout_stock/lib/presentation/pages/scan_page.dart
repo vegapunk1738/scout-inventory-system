46a06da6350295b67cf859a19fbaddaba9b08d5a
Rony <ronymawad@gmail.com>
2026-02-20 10:55:21 -0500

    Revert "Update scan_page.dart"
    This reverts commit 33f652f2cd6cd80b3927fbc5473085e619b2e807.

--- app/scout_stock/lib/presentation/pages/scan_page.dart
+++ app/scout_stock/lib/presentation/pages/scan_page.dart
@@ -443,19 +443,10 @@ class _ScanPageState extends State<ScanPage>
             fit: StackFit.expand,
             children: [
               // Keep scanner widget mounted (smoother switching)
-              Positioned.fill(
-                child: MediaQuery.removePadding(
-                  context: context,
-                  removeTop: true,
-                  removeBottom: true,
-                  removeLeft: true,
-                  removeRight: true,
-                  child: RepaintBoundary(
-                    child: MobileScanner(
-                      controller: _controller,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
+              RepaintBoundary(
+                child: MobileScanner(
+                  controller: _controller,
+                  fit: BoxFit.cover,
                 ),
               ),
 
